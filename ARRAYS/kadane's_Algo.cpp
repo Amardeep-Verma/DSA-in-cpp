6b95b61da9788144c0faf2fdc53cf3e432c5f938
Amardeep <vermaamardeep86@gmail.com>
2024-09-19 21:45:23 +0530

    Added

---
+++ ARRAYS/kadane's_Algo.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+void maxSubarraySum(int *arr, int n)
+{
+  int maxSum = INT_MIN;
+  int currSum = 0;
+
+  for (int i = 0; i < n; i++)
+  {
+    currSum += arr[i];
+    maxSum = max(currSum, maxSum);
+    if (currSum < 0)
+    {
+      currSum = 0;
+    }
+  }
+  cout << "Maximum subarray sum = " << maxSum;
+}
+
+int main()
+{
+  int arr[6] = {2, -3, 6, -5, 4, 2};
+  int n = sizeof(arr) / sizeof(int);
+
+  maxSubarraySum(arr, n);
+}
